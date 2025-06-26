9ce9f8ced56f6e8291c44b53513f0af16bf4e9a9
HyukgiLee <hkgilee@gmail.com>
2025-06-25 22:45:43 -0400

    find absolute value (two versions)

--- Ch.1/find_abs.py
+++ Ch.1/find_abs.py
@@ -1,1 +1,26 @@
-# asd
+# import library
+import math
+
+# find sign of absolute variable
+# input : integer number(int)
+# output : absolute value of number(int)
+
+def abs_sign(num):
+    if num >= 0:
+        return num
+    else:
+        return -num
+    
+# find absolute value using math function(math.sqrt)
+# input : integer number(int)
+# output : absolute value of the input(int)
+
+def abs_square(num):
+    num_sq = num* num
+    return int(math.sqrt(num_sq))
+
+print(abs_sign(5))
+print(abs_sign(-3))
+print()
+print(abs_square(7))
+print(abs_square(-4))
